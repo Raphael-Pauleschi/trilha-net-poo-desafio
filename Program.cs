925abb8b3337874addcf6c65d5c0fe4f5ade758f
Raphael-Pauleschi <Raphael.f.pauleschi@gmail.com>
2024-07-30 15:55:08 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new("(83) 3121-4746","IPhone 11","356656422302496", 64);
+Nokia nokia = new("(65) 3299-9284","Nokia C01 Plus","466657422302099", 35);
+
+iphone.Ligar();
+nokia.Ligar();
+
+iphone.ReceberLigacao();
+nokia.ReceberLigacao();
+
+iphone.InstalarAplicativo("Angry Birds 2");
+nokia.InstalarAplicativo("Nubank");
